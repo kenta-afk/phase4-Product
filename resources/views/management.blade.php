aea90fb9c9b416926463a1d26a549777efb52ded
kenta-afk <kentarou9.60@icloud.com>
2024-12-01 19:33:02 +0900

    いい感じにデザインした

--- resources/views/management.blade.php
+++ resources/views/management.blade.php
@@ -2,30 +2,71 @@
 <html>
 <head>
     <title>管理者画面</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">管理者画面</h1>
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:text-center">
+        <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">管理者画面</p>
         @if (session('success')) 
             <div class="alert alert-success"> 
                 {{ session('success') }} 
             </div> 
         @endif
-        <div class="row">
-            <div class="col-md-3 mb-3">
-                <a href="{{ route('appointments.create') }}" class="btn btn-primary btn-block">アポ情報新規登録</a>
+      </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+         <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            <div class="relative pl-20 bg-white border border-gray-200 rounded-lg shadow-lg px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+              <dt class="text-base/7 font-semibold text-gray-900">
+                <div class="absolute left-1 top-1 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" viewBox="0 0 16 16" fill="currentColor">
+                    <path d="M4.5 13a3.5 3.5 0 0 1-1.41-6.705A3.5 3.5 0 0 1 9.72 4.124a2.5 2.5 0 0 1 3.197 3.018A3.001 3.001 0 0 1 12 13H4.5Zm.72-5.03a.75.75 0 0 0 1.06 1.06l.97-.97v2.69a.75.75 0 0 0 1.5 0V8.06l.97.97a.75.75 0 1 0 1.06-1.06L8.53 5.72a.75.75 0 0 0-1.06 0L5.22 7.97Z" clip-rule="evenodd" />
+                  </svg>
+                </div>
+                <a href="{{ route('appointments.create') }}" class="btn btn-primary btn-block">アポイントメント登録</a>
+              </dt>
+              <dd class="mt-2 text-base/7 text-gray-600 mb-4">アポイントメント情報の登録をします。</dd>
             </div>
-            <div class="col-md-3 mb-3">
-                <a href="/appointments" class="btn btn-primary btn-block">アポ情報一覧</a>
+
+            <div class="relative pl-20 bg-white border border-gray-200 rounded-lg shadow-lg px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+              <dt class="text-base/7 font-semibold text-gray-900">
+                <div class="absolute left-1 top-1 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="size-6 text-white" viewBox="0 0 16 16" fill="currentColor">
+                    <path d="M3 4.75a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM6.25 3a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5h-7ZM6.25 7.25a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5h-7ZM6.25 11.5a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5h-7ZM4 12.25a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM3 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" />
+                  </svg>
+                </div>
+                <a href="/appointments" class="btn btn-primary btn-block">アポイントメント情報一覧</a>
+              </dt>
+              <dd class="mt-2 text-base/7 text-gray-600 mb-4">アポイントメント情報の一覧を表示します。</dd>
             </div>
-            <div class="col-md-3 mb-3">
-                <a href="/visitors" class="btn btn-primary btn-block">来客者情報一覧</a>
-            </div>
-            <div class="col-md-3 mb-3">
-                <a href="/reception" class="btn btn-primary btn-block">受付画面に移動</a>
-            </div>
-        </div>
+          <div class="relative pl-20 bg-white border border-gray-200 rounded-lg shadow-lg px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+            <dt class="text-base/7 font-semibold text-gray-900">
+              <div class="absolute left-1 top-1 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                <svg xmlns="http://www.w3.org/2000/svg" class="size-6 text-white" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                    <path d="M8.5 4.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10.9 12.006c.11.542-.348.994-.9.994H2c-.553 0-1.01-.452-.902-.994a5.002 5.002 0 0 1 9.803 0ZM14.002 12h-1.59a2.556 2.556 0 0 0-.04-.29 6.476 6.476 0 0 0-1.167-2.603 3.002 3.002 0 0 1 3.633 1.911c.18.522-.283.982-.836.982ZM12 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
+                </svg>
+              </div>
+              <a href="/visitors" class="btn btn-primary btn-block">来客者情報一覧</a>
+            </dt>
+            <dd class="mt-2 text-base/7 text-gray-600 mb-4">訪問された方の一覧を表示します。</dd>
+          </div>
+          <div class="relative pl-20 bg-white border border-gray-200 rounded-lg shadow-lg px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+            <dt class="text-base/7 font-semibold text-gray-900">
+              <div class="absolute left-1 top-1 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                <svg xmlns="http://www.w3.org/2000/svg" class="size-6 text-white" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                    <path d="M2 8c0 .414.336.75.75.75h8.69l-1.22 1.22a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06l-2.5-2.5a.75.75 0 1 0-1.06 1.06l1.22 1.22H2.75A.75.75 0 0 0 2 8Z" clip-rule="evenodd" />
+                </svg>
+
+              </div>
+              <a href="/reception" class="btn btn-primary btn-block">受付画面に移動</a>
+            </dt>
+            <dd class="mt-2 text-base/7 text-gray-600 mb-4">受付画面は、来客時にお客様が操作する画面です。</dd>
+          </div>
+         </dl>
+      </div>
     </div>
+  </div>
 </body>
 </html>
+
