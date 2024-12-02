7292cc14fb2da2e7f6478c0ba6e358e18f1b9399
KOU050223 <kouhei20050223@icloud.com>
2024-12-03 01:02:18 +0900

    add Slackのワークスペースメンバー(name/email/slackid)を取得する機能を追加

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -24,9 +24,34 @@ class SlackController extends Controller
         ]);
 
         if ($response->successful()) {
-            return response()->json(['status' => 'Message sent successfully']);
+            return response()->json(['status' => '担当者を呼び出しました'], 200);
         } else {
             return response()->json(['status' => 'Failed to send message', 'error' => $response->json()], 500);
+        }
+    }
+    public function getWorkspaceMembers()
+    {
+        $token = $this->slackToken;
+
+        $response = Http::withToken($token)->get('https://slack.com/api/users.list');
+
+        // dd($response->json());
+        if ($response->successful()) {
+            $members = $response->json()['members'];
+            $memberIds = array_filter(array_map(function($member) {
+            if (!$member['is_bot']) {
+                return [
+                    'host_name' => $member['real_name'],
+                    'host_email' => $member['profile']['email'] ?? null,
+                    'slack_id' => $member['id'],
+                ]; // スラックID・名前・Emailを取得
+                // DBに保存する処理を追加する
+            }
+            return null;
+            }, $members));
+            return response()->json(['members' => $memberIds], 200, [], JSON_UNESCAPED_UNICODE);
+        } else {
+            return response()->json(['status' => 'Failed to retrieve members', 'error' => $response->json()], 500);
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\SlackController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/send-message', [SlackController::class, 'sendMessage']);
-Route::get('/workspace-members', [SlackController::class,'getWorkspaceMembers']);
+Route::post('/send-message', [SlackController::class, 'sendMessage']);// Slackにメッセージを送信するby魚住
+Route::get('/workspace-members', [SlackController::class,'getWorkspaceMembers']);// Slackのワークスペースメンバーを取得するby魚住
 Route::get('/messagetest', function () {
     return view('messagetest');
-});
+});// メッセージテスト用のビューを返すby魚住
