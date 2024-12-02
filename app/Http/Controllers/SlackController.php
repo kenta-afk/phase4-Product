be218d6c59c6cd80e37489497a2d10e49fff6e28
KOU050223 <kouhei20050223@icloud.com>
2024-12-03 01:32:56 +0900

    add Slackメンバー情報をDBに保存する処理 追加

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Models\Host;
 
 class SlackController extends Controller
 {
@@ -39,15 +40,22 @@ class SlackController extends Controller
         if ($response->successful()) {
             $members = $response->json()['members'];
             $memberIds = array_filter(array_map(function($member) {
-            if (!$member['is_bot']) {
-                return [
-                    'host_name' => $member['real_name'],
-                    'host_email' => $member['profile']['email'] ?? null,
-                    'slack_id' => $member['id'],
-                ]; // スラックID・名前・Emailを取得
-                // DBに保存する処理を追加する
-            }
-            return null;
+                if (!$member['is_bot']) {
+                    $hostData = [
+                        'host_name' => $member['real_name'],
+                        'slack_id' => $member['id'],
+                        // 'host_email' => $member['profile']['email'] ?? null, //DBの定義になかったためいったんコメントアウト
+                    ];
+
+                    // DBに保存する処理を追加する
+                    Host::updateOrCreate(
+                        ['slack_id' => $hostData['slack_id']],
+                        $hostData
+                    );
+
+                    return $hostData;
+                }
+                return null;
             }, $members));
             return response()->json(['members' => $memberIds], 200, [], JSON_UNESCAPED_UNICODE);
         } else {
