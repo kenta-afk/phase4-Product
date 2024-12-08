0ecd789ca3a7394e7617200f6e71a76788393737
nitamagoGoGo <nitamagorain@gmail.com>
2024-12-08 16:02:39 +0900

    来客者情報一覧表示機能追加

--- resources/views/appointments/create.blade.php
+++ resources/views/appointments/create.blade.php
@@ -44,7 +44,7 @@
           <div class="mt-2.5">
             <select name="room_id" id="room_id" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" required>
               @foreach($rooms as $room)
-                <option value="{{ $room->id }}">{{ $room->room_name }}</option>
+                <option value="{{ $room->id }}">{{ $room->name }}</option>
               @endforeach
             </select>
           </div>
--- resources/views/visitors/index.blade.php
+++ resources/views/visitors/index.blade.php
@@ -1,3 +1,41 @@
-<h1>ここには、すでに会議が終了した人の一覧が表示されます。</h1>
-
-<h2>appointmentsテーブルのstatusのカラムが1（すでに会議終了）したレコードの一覧を表示します。</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>来客者情報一覧</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">来客者情報一覧</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>来客者の名前</th>
+                    <th>来客者の所属</th>
+                    <th>対応者の名前</th>
+                    <th>会議室の名前</th>
+                    <th>訪問日時</th>
+                    <th>要件</th>
+                    <th>アクション</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($appointments as $appointment)
+                    <tr>
+                        <td>{{ $appointment->visitor_name }}</td>
+                        <td>{{ $appointment->visitor_company }}</td>
+                        <td>{{ $appointment->users->first()->name }}</td>
+                        <td>{{ $appointment->room->room_name }}</td>
+                        <td>{{ $appointment->date }}</td>
+                        <td>{{ $appointment->comment }}</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-info">編集</a>
+                            <a href="#" class="btn btn-sm btn-danger">削除</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
